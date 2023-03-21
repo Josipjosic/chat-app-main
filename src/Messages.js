7b0de665bc572dee293498b5ab2249afc9352725
Josip Josic <josip.josic1911@hotmail.com>
2023-03-21 02:25:14 +0100

    wChat app

---
+++ src/Messages.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useRef, useEffect } from "react";
+
+function Messages({messages, currentMember}) {
+  const bottomRef = useRef(null);
+
+  //Called useEffect so when messages update, scroll behavior can trigger 
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({behavior: 'smooth'})
+  }, [messages])
+
+  //Main function and logic for displaying messages while mapping through messages array
+  function renderMessage(message) {
+    const { data, member } = message;
+    const messageFromMe = member.id === currentMember.id;
+    const className = messageFromMe
+      ? "Messages-message currentMember"
+      : "Messages-message";
+      console.log(message)
+    return (
+      <li className={className} key={Math.random()}>
+        <span
+          className="avatar"
+          style={{ backgroundColor: member.clientData.color }}
+        />
+        <div className="Message-content">
+          <div className="username">{member.clientData.username}</div>
+          <div className="text" ref={bottomRef}>{data}</div>
+        </div>
+      </li>
+    );
+  }
+
+  return (
+   <ul className="Messages-list">{messages.map((m) => renderMessage(m))}</ul>
+  );
+}
+
+export default Messages;
